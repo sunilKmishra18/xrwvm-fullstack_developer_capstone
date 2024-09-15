182185f7d154449707d0027137b09bb3761a5e66
sunilKmishra18 <sunilmishra18.creativity@gmail.com>
2024-09-15 02:47:06 -0400

    Added Code changes for Development5

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY =\
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+APPEND_SLASH = False
 
 ALLOWED_HOSTS=['localhost','https://sunilmishra2-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS=['https://sunilmishra2-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
